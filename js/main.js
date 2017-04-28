361bdccdb05c18580ac285f5b610e7ee44e34415
lor104 <lauren.rosentzveig@gmail.com>
2017-04-28 16:16:55 -0400

    When switch is clicked, text changes

--- js/main.js
+++ js/main.js
@@ -3,8 +3,15 @@ $(document).ready(function() {
   $('button.switch').on('click', function() {
     $(this).toggleClass( 'on' );
     $(this).toggleClass( 'off' );
+
     $('body').toggleClass( 'light' );
-    $('body').toggleClass( 'dark' ); 
+    $('body').toggleClass( 'dark' );
+
+    if ( $(this).hasClass("off") ) {
+      $('h1.status').text("Hey, who turned off the lights")
+}   else {
+      $('h1.status').text("It's so bright in here!")
+}
 
 
   });
